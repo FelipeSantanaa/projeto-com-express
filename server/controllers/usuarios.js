d1eebe1c0a5bcf7f74896109440cb797f4083072
Felipe Santana <felipe008_@hotmail.com>
2021-09-13 20:55:24 -0300

    feature: Adicionando tela dos usuarios

--- server/controllers/usuarios.js
+++ server/controllers/usuarios.js
@@ -1,14 +1,23 @@
 const usuariosPlaceholder = require('../data/usuariosPlaceholder.json')
 
 const controller = {
-    index: function(req, res, next) {
-        res.render('usuarios', { 
-            titulo: 'Usuários',
-            subtitulo: 'Você está na página de usuários',
-            usuarios: usuariosPlaceholder
-        });
-      }
-
+  index: (req, res, next) => {
+    res.render('usuarios', {
+      titulo: 'Usuários',
+      subtitulo: 'Você está na página de usuários',
+      usuarios: usuariosPlaceholder
+    });
+  },
+  show: (req, res, next) => {
+    const { id } = req.params
+    const usuario = usuariosPlaceholder[id]
+    res.render('usuario', {
+      titulo: 'Usuário',
+      subtitulo: `Você está na página do usuário ${id}`,
+      usuario,
+      bannerTopo: '/images/banner-topo-usuario-1564x472.png'
+    });
+  }
 }
 
 module.exports = controller
